9e8a76fed2830f5e95f94f5c7d7ccbcd81d36bf6
Kekeli <114351095+KekeliEinstein@users.noreply.github.com>
2025-07-18 07:15:09 +0000

    Exit Menu Implementation
    User can now exit the app using the close button. It shows a choice panel

--- Assets/scripts/Monobehavior/QuitWindows.cs
+++ Assets/scripts/Monobehavior/QuitWindows.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class QuitWindows : MonoBehaviour
 {
+    public GameObject quitDialog; // Assign in Inspector
 
     public void Start()
     {
         {
             Input.backButtonLeavesApp = true;
+            if (quitDialog != null)
+                quitDialog.SetActive(false); // Hide dialog at start
         }
         // Optionally, you can add any initialization code here
     }
+
+    // Call this method to show the quit dialog
+    public void ShowQuitDialog()
+    {
+        if (quitDialog != null)
+            quitDialog.SetActive(true);
+    }
+    // Call this method to hide the quit dialog
     public void QuitGame()
     {
        // if (Input.GetKey("escape"))
@@ -19,5 +30,11 @@ public class QuitWindows : MonoBehaviour
             //Application.Quit();
             QuitApplicationUtility.MoveAndroidApplicationToBack();
         }
+    }
+    // Called when "Cancel" button is pressed
+    public void CancelQuit()
+    {
+        if (quitDialog != null)
+            quitDialog.SetActive(false);
     }
 }
